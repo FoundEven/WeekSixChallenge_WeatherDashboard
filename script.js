30d8a2f602c3f7b37c942c3fc8ca7a531ed9718b
Evan Rice <evancr2017@gmail.com>
2023-10-02 22:04:18 -0500

    Added icons to weather forcast

--- script.js
+++ script.js
@@ -8,26 +8,31 @@ var windy = document.querySelector("#wind");
 var humid = document.querySelector("#hum");
 //These are for the first forcast day
 var date = document.querySelector("#date1");
+var iconEl1 = document.querySelector("#ic1");
 var tem = document.querySelector("#temp1");
 var wimdy = document.querySelector("#wind1");
 var hum = document.querySelector("#hum1");
 //These are for the second forcast day
 var date1 = document.querySelector("#date2");
+var iconEl2 = document.querySelector("#ic2");
 var tem1 = document.querySelector("#temp2");
 var wimdy1 = document.querySelector("#wind2");
 var hum1 = document.querySelector("#hum2");
 //These are for the third forcast day
 var date2 = document.querySelector("#date3");
+var iconEl3 = document.querySelector("#ic3");
 var tem2 = document.querySelector("#temp3");
 var wimdy2 = document.querySelector("#wind3");
 var hum2 = document.querySelector("#hum3");
 //These are for the fourth forcast day
 var date3 = document.querySelector("#date4");
+var iconEl4 = document.querySelector("#ic4");
 var tem3 = document.querySelector("#temp4");
 var wimdy3 = document.querySelector("#wind4");
 var hum3 = document.querySelector("#hum4");
 //These are for the fivth forcast day
 var date4 = document.querySelector("#date5");
+var iconEl5 = document.querySelector("#ic5");
 var tem4 = document.querySelector("#temp5");
 var wimdy4 = document.querySelector("#wind5");
 var hum4 = document.querySelector("#hum5");
@@ -70,10 +75,9 @@ function getFetch(lat, lon) {
 
         return response.json();
     }).then(function (data) {
-        console.log(data);
         var iconId = data.weather[0].icon;
-        iconEl.style.content = iconId;
-        console.log(iconEl);
+        var iconU =  "https://openweathermap.org/img/w/" + iconId + ".png";
+        iconEl.setAttribute("src", iconU);
         cityName.textContent = data.name + "(Today)" ;
         tempiture.textContent = data.main.temp + " F";
         windy.textContent = data.wind.speed + " mph";
@@ -86,28 +90,43 @@ function getFetch(lat, lon) {
         return response.json();
     }).then(function (data) {
 
-    
+        //day 1
         date.textContent = "(" + data.list[3].dt_txt + ')';
+        var iconId1 = data.list[3].weather[0].icon;
+        var iconU1 =  "https://openweathermap.org/img/w/" + iconId1 + ".png";
+        iconEl1.setAttribute("src", iconU1);
         tem.textContent = data.list[3].main.temp + " F";
         wimdy.textContent = data.list[3].wind.speed + " mph";
         hum.textContent = data.list[3].main.humidity + '%';
-
+        //day 2
         date1.textContent = "(" + data.list[11].dt_txt + ')';
+        var iconId2 = data.list[11].weather[0].icon;
+        var iconU2 =  "https://openweathermap.org/img/w/" + iconId2 + ".png";
+        iconEl2.setAttribute("src", iconU2);
         tem1.textContent = data.list[11].main.temp + " F";
         wimdy1.textContent = data.list[11].wind.speed + " mph";
         hum1.textContent = data.list[11].main.humidity + '%';
-
+        //day 3
         date2.textContent = "(" + data.list[19].dt_txt + ')';
+        var iconId3 = data.list[19].weather[0].icon;
+        var iconU3 =  "https://openweathermap.org/img/w/" + iconId3 + ".png";
+        iconEl3.setAttribute("src", iconU3);
         tem2.textContent = data.list[19].main.temp + " F";
         wimdy2.textContent = data.list[19].wind.speed + " mph";
         hum2.textContent = data.list[19].main.humidity + '%';
-
+        //day 4
         date3.textContent = "(" + data.list[27].dt_txt + ')';
+        var iconId4 = data.list[27].weather[0].icon;
+        var iconU4 =  "https://openweathermap.org/img/w/" + iconId4 + ".png";
+        iconEl4.setAttribute("src", iconU4);
         tem3.textContent = data.list[27].main.temp + " F";
         wimdy3.textContent = data.list[27].wind.speed + " mph";
         hum3.textContent = data.list[27].main.humidity + '%';
-
+        //day 5
         date4.textContent = "(" + data.list[35].dt_txt + ')';
+        var iconId5 = data.list[35].weather[0].icon;
+        var iconU5 =  "https://openweathermap.org/img/w/" + iconId5 + ".png";
+        iconEl5.setAttribute("src", iconU5);
         tem4.textContent = data.list[35].main.temp + " F";
         wimdy4.textContent = data.list[35].wind.speed + " mph";
         hum4.textContent = data.list[35].main.humidity + '%';
